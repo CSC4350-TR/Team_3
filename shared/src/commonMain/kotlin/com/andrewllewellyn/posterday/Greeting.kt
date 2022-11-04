0f011cd6995ed5af12eb696a716337d3d7bb607d
alwynGSU <89497284+alwynGSU@users.noreply.github.com>
2022-11-04 12:58:28 -0400

    First Commit

---
+++ shared/src/commonMain/kotlin/com/andrewllewellyn/posterday/Greeting.kt
@@ -0,0 +1,9 @@
+package com.andrewllewellyn.posterday
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greeting(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
