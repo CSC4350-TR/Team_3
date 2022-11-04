aa6d1cc57262015910eddfc5277067ee964ff319
allewellyn4 <89497284+alwynGSU@users.noreply.github.com>
2022-11-04 13:39:01 -0400

    Delete shared directory

--- shared/src/commonMain/kotlin/com/andrewllewellyn/posterday/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.andrewllewellyn.posterday
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greeting(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
